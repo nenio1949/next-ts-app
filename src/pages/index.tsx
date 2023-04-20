f45d01414c44e5548286f24d60b8a57abaddcf31
yong.li <yong.li@funenc.com>
2023-04-20 17:37:55 +0800

    style: 样式文件分割

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   utils.operate.setDocumentTitle('首页')
 
   return (
-    <div className="d-container" style={{ height: '100%', backgroundColor: '#ffffff' }}>
+    <div className="d-container" style={{ height: '100%' }}>
       <div className="d-container-body" style={{ textAlign: 'center' }}>
         <Image src={WelcomeImg} height={300} alt="" />
         <div style={{ padding: 5, fontSize: 20 }}>欢迎访问用户权益管理平台</div>
